5c488a56220ace32b8cf2aeeaf65a59660d717bd
pveenstra <pveenstra@G100695>
2017-01-09 16:28:23 +0000

    another test

--- Triad.Intranet.Provisioning/Triad.Provisioning.Console/Install.ps1
+++ Triad.Intranet.Provisioning/Triad.Provisioning.Console/Install.ps1
@@ -53,7 +53,7 @@ $web = Get-PnPWeb
 dir "$path\Templates"
 
 
-Apply-PnPProvisioningTemplate -Web $web -Path "$path\Templates\Home.xml" -ResourceFolder "$path\Templates"
+Apply-PnPProvisioningTemplate -Web $web -Path "$path\Templates\Home.xml"
 
 Write-Host "Applied"
 
